c408aabfe18b88f7aa230bf18715affb2ffc389b
Jiubei0408 <757795210@qq.com>
2021-08-10 07:55:00 +0800

    hide accounts

--- app/spiders/zju_domjudge_spider.py
+++ app/spiders/zju_domjudge_spider.py
@@ -1,27 +1,8 @@
 from app.spiders.domjudge_spider import DomjudgeSpider
+from app.config.accounts import zju_domjudge_accounts
 
 
 class ZjuDomjudgeSpider(DomjudgeSpider):
     oj_name = 'zju-domjudge'
     base_url = 'http://acm.zju.edu.cn/domjudge'
-    accounts = [
-        ('team-701', 'hxcvqh'),
-        ('team-702', 'harbmn'),
-        ('team-703', 'b9wh6b'),
-        ('team-704', 'zc7uvf'),
-        ('team-705', 'qsgm99'),
-        ('team-706', 'br4zzz'),
-        ('team-707', 'cpc6de'),
-        ('team-708', 'mzxjzw'),
-        ('team-709', '7jy54a'),
-        ('team-710', 'srdja8'),
-        ('team-711', '6ygyht'),
-        ('team-712', '5ra988'),
-        ('team-713', 'f83gqh'),
-        ('team-714', 'jj3we6'),
-        ('team-715', '2kmqmv')
-    ]
-
-
-if __name__ == '__main__':
-    print(ZjuDomjudgeSpider('team-702', 'harbmn').get_contest_meta(''))
+    accounts = zju_domjudge_accounts
